9af299d2d45933dd54c62949530e3caa3edfb596
Vignesh Natarajan <vickyemailbox@gmail.com>
2021-01-24 16:04:33 +0100

    xml parser makefile

--- src/xml_parser.c
+++ src/xml_parser.c
@@ -1,8 +1,37 @@
-/* Simple XML Parser  
- * 
- *              Author : Viki (a) Vignesh Natarajan
- *              Firma  : vikilabs.org
+/**
+ *
+ * @FILE        	: XML_PARSER.C 
+ * @DESCRIPTION 	: LIBRARY TO PARSE XML FILE
+ *
+ * @AUTHOR      	: VIKI (A) VIGNESH NATARAJAN
+ * @CONTACT         	: VIKILABS.ORG
+ *
  */
+
+/*
+    Copyright (c) 2016 Viki( Vignesh Natarajan )
+
+    Contact @ vikilabs.org
+
+    Permission is hereby granted, free of charge, to any person obtaining
+    a copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the Software
+    is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+    OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #include "xml_parser.h"
 
 char *get_xml_tag_value(const char *xml_file, char *xml_tag)
